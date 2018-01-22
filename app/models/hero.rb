74e4f41142f532c4853beb2e8a89f1090d1acd41
Brittany Morris <brittmmendez@gmail.com>
2018-01-22 19:46:21 +0000

    automatically backed up by learn

---
+++ app/models/hero.rb
@@ -0,0 +1,23 @@
+class Hero
+
+  attr_reader :name, :power, :bio
+
+  HEROS=[]
+
+  def initialize(args)
+    @name=args[:name]
+    @power=args[:power]
+    @bio=args[:bio]
+    HEROS<<self
+  end
+
+  def self.all
+    HEROS
+  end
+
+  def self.clear
+    HEROS.clear
+  end
+
+
+end
